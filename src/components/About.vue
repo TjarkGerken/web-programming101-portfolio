f1a164c73eeb5fa538e66bb56934be366c202cbf
TjarkGerken <96004366+TjarkGerken@users.noreply.github.com>
2023-11-15 12:01:27 +0100

    init commit with wip rout

---
+++ src/components/About.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-4xl text-amber-50">About</h1>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-4xl text-amber-50">Home</h1>
+</template>
