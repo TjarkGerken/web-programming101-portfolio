a2ae306e63dfa23102d6d40489b5b34ec279e35f
TjarkGerken <96004366+TjarkGerken@users.noreply.github.com>
2023-11-30 17:23:46 +0100

    fixed bug

--- src/components/utils/Navbar.vue
+++ src/components/utils/Navbar.vue
@@ -1,21 +1,35 @@
 <template>
   <header class="bg-transparent shadow-[rgba(17,_17,_26,_0.1)_0px_0px_8px]">
     <nav
-      class=" flex items-center justify-between p-6 lg:px-8"
+      class="flex items-center justify-between p-6 lg:px-8"
       aria-label="Global"
     >
       <div class="flex lg:flex-1 2xl:ml-48">
-        <router-link to="" class="flex items-center -m-1.5 p-1.5">
+        <router-link to="" class="-m-1.5 flex items-center p-1.5">
           <span class="sr-only">Your Company</span>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 378 375" fill="none" class="h-[25px] w-[25px] 2xl:h-[35px] 2xl:w-[35px]">
-            <path d="M132 0L181 28V352.5L132 325.5V0Z" fill="black"/>
-            <path d="M197.5 121.5L246.5 149V344L197.5 316.5V121.5Z" fill="black"/>
-            <path d="M263 51.5L312 78.5V331.5L263 304.5V51.5Z" fill="black"/>
-            <path d="M329 144.5L377.5 172.5V322.5L329 295.5V144.5Z" fill="black"/>
-            <path d="M0.5 210.5L49.5 238.5V375L0.5 348V210.5Z" fill="black"/>
-            <path d="M66.5 103.5L115.5 130V365L66.5 337.5V103.5Z" fill="black"/>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 378 375"
+            fill="none"
+            class="h-[25px] w-[25px] 2xl:h-[35px] 2xl:w-[35px]"
+          >
+            <path d="M132 0L181 28V352.5L132 325.5V0Z" fill="black" />
+            <path
+              d="M197.5 121.5L246.5 149V344L197.5 316.5V121.5Z"
+              fill="black"
+            />
+            <path d="M263 51.5L312 78.5V331.5L263 304.5V51.5Z" fill="black" />
+            <path
+              d="M329 144.5L377.5 172.5V322.5L329 295.5V144.5Z"
+              fill="black"
+            />
+            <path d="M0.5 210.5L49.5 238.5V375L0.5 348V210.5Z" fill="black" />
+            <path
+              d="M66.5 103.5L115.5 130V365L66.5 337.5V103.5Z"
+              fill="black"
+            />
           </svg>
-          <div class="ml-2 text-md font-bold 2xl:text-xl">Polar Peak</div>
+          <div class="text-md ml-2 font-bold 2xl:text-xl">Polar Peak</div>
         </router-link>
       </div>
 
@@ -38,7 +52,7 @@
         >
         <router-link
           to="/about"
-          class="text-xl font-semibold leading-6 text-gray-900 "
+          class="text-xl font-semibold leading-6 text-gray-900"
           >About</router-link
         >
       </PopoverGroup>
@@ -83,20 +97,35 @@
       @close="mobileMenuOpen = false"
       :open="mobileMenuOpen"
     >
-      <div class="fixed inset-0 z-10" />
+      <div class="fixed inset-0 z-30" />
       <DialogPanel
-        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+        class="fixed inset-y-0 right-0 z-30 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
       >
         <div class="flex items-center justify-between">
-          <router-link to="#" class="flex items-center -m-1.5 p-1.5">
+          <router-link to="#" class="-m-1.5 flex items-center p-1.5">
             <span class="sr-only">Your Company</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 378 375" fill="none">
-              <path d="M132 0L181 28V352.5L132 325.5V0Z" fill="black"/>
-              <path d="M197.5 121.5L246.5 149V344L197.5 316.5V121.5Z" fill="black"/>
-              <path d="M263 51.5L312 78.5V331.5L263 304.5V51.5Z" fill="black"/>
-              <path d="M329 144.5L377.5 172.5V322.5L329 295.5V144.5Z" fill="black"/>
-              <path d="M0.5 210.5L49.5 238.5V375L0.5 348V210.5Z" fill="black"/>
-              <path d="M66.5 103.5L115.5 130V365L66.5 337.5V103.5Z" fill="black"/>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              viewBox="0 0 378 375"
+              fill="none"
+            >
+              <path d="M132 0L181 28V352.5L132 325.5V0Z" fill="black" />
+              <path
+                d="M197.5 121.5L246.5 149V344L197.5 316.5V121.5Z"
+                fill="black"
+              />
+              <path d="M263 51.5L312 78.5V331.5L263 304.5V51.5Z" fill="black" />
+              <path
+                d="M329 144.5L377.5 172.5V322.5L329 295.5V144.5Z"
+                fill="black"
+              />
+              <path d="M0.5 210.5L49.5 238.5V375L0.5 348V210.5Z" fill="black" />
+              <path
+                d="M66.5 103.5L115.5 130V365L66.5 337.5V103.5Z"
+                fill="black"
+              />
             </svg>
             <div class="ml-2 font-bold">Polar Peak</div>
           </router-link>
--- src/views/landingpage/HomeView.vue
+++ src/views/landingpage/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
-  <Hero class="z-10"></Hero>
+  <Navbar class="z-30"></Navbar>
+  <Hero class="z-0"></Hero>
 </template>
 <script setup>
 import Hero from "@/components/landing-page/Hero.vue";
+import Navbar from "@/components/utils/Navbar.vue";
 </script>
