18de149bd7494d5408f73378894a8d0ac6e9801d
Tjark Gerken <tjark.gerken@accenture.com>
2023-12-06 12:01:36 +0100

    added handling for non existent users

--- src/views/app/ActivitiesView.vue
+++ src/views/app/ActivitiesView.vue
@@ -4,18 +4,34 @@ import ActivitiesCard from "@/components/application/ActivitiesCard.vue";
 import { onMounted, ref } from "vue";
 import { getExercises, getPolarActivities } from "@/api/getPolarActivities";
 import Spinner from "@/components/utils/Spinner.vue";
+import store from "@/store";
 
 const exercises = ref([]);
 const isLoading = ref(true);
+const noActivities = ref(false);
 onMounted(() => {
-  getPolarActivities().then(async () => {
-    getExercises()
-      .then((res) => {
-        exercises.value = res;
-        isLoading.value = false;
-      })
-      .then(() => (exercises.value = [...exercises.value].reverse()));
-  });
+  try {
+    if (store.state.user.polar_user) {
+      getPolarActivities().then(async () => {
+        getExercises()
+          .then((res) => {
+            if (res) {
+              exercises.value = res;
+              isLoading.value = false;
+            } else {
+              noActivities.value = true;
+              isLoading.value = false;
+            }
+          })
+          .then(() => (exercises.value = [...exercises.value].reverse()));
+      });
+    } else {
+      noActivities.value = true;
+      isLoading.value = false;
+    }
+  } catch (e) {
+    console.warn(e);
+  }
 });
 </script>
 
@@ -23,6 +39,13 @@ onMounted(() => {
   <ApplicationNavbar></ApplicationNavbar>
   <div class="h-fit w-screen px-8 lg:ml-32 lg:h-screen lg:w-[calc(100%-128px)]">
     <h1 class="w-fit pl-12 pt-12 text-left text-3xl">Your Activities</h1>
+    <div v-if="noActivities" class="w-fit pl-12 pt-12 text-left">
+      You got no Activities.
+      <router-link to="/profile" class="underline"
+        >Connect your Polar Account</router-link
+      >
+      to get started or upload a new Activity if you are already connected!
+    </div>
     <div class="flex h-fit w-full flex-col items-center gap-4">
       <ActivitiesCard
         class="w-11/12 lg:w-8/12"
@@ -32,8 +55,11 @@ onMounted(() => {
         :activity="exercise"
       />
     </div>
-    <div v-if="!(exercises.length > 0) & !isLoading">You got no Activities</div>
-    <div v-if="isLoading" class="flex w-full items-center justify-center">
+
+    <div
+      v-if="isLoading && !noActivities"
+      class="flex w-full items-center justify-center"
+    >
       <Spinner />
     </div>
   </div>
--- src/views/app/DashboardView.vue
+++ src/views/app/DashboardView.vue
@@ -12,19 +12,32 @@ import Stats from "@/components/application/Stats.vue";
 import store from "../../store";
 
 const exercises = ref({});
+const isLoading = ref(true);
+const noActivities = ref(false);
 const lastWeekStats = ref({});
 onMounted(() => {
-  getPolarActivities().then(async () => {
-    getLatestExercise()
-      .then((res) => {
-        exercises.value = res;
-      })
-      .then(
-        await aggregateLastWeekStats().then((res) => {
-          lastWeekStats.value = res;
-        }),
-      );
-  });
+  try {
+    if (store.state.user.polar_user) {
+      getPolarActivities().then(async () => {
+        getLatestExercise()
+          .then((res) => {
+            exercises.value = res;
+          })
+          .then(
+            await aggregateLastWeekStats().then((res) => {
+              lastWeekStats.value = res;
+            }),
+          );
+      });
+    } else {
+      noActivities.value = true;
+      isLoading.value = false;
+    }
+  } catch (e) {
+    noActivities.value = true;
+    isLoading.value = false;
+    console.warn(e);
+  }
 });
 </script>
 
@@ -50,17 +63,31 @@ onMounted(() => {
           v-if="Object.keys(exercises).length > 0"
           :activity="exercises"
         />
+        <div v-if="noActivities" class="text-center">
+          You got no Activities.
+          <router-link to="/profile" class="underline"
+            >Connect your Polar Account</router-link
+          >
+          to get started or upload a new Activity if you are already connected!
+        </div>
       </div>
       <div class="flex flex-col lg:flex-row">
         <div class="flex flex-col lg:w-1/2">
           <h2 class="mt-20 text-center text-3xl font-semibold lg:text-left">
             Weekly Progress
           </h2>
-          <p class="mb-20 text-center lg:text-left">
+          <p class="mb-20 text-center lg:text-left" v-if="!noActivities">
             Congratulations, You’ve burned {{ lastWeekStats.total_calories }}
             Calories this week. Keep it up!.
           </p>
-          <div class="flex flex-col lg:flex-row">
+          <p
+            class="mb-20 text-center lg:w-3/4 lg:text-left"
+            v-if="noActivities"
+          >
+            Here you will find your weekly progress as soon as you connected
+            your Polar Account and uploaded your first Activity.
+          </p>
+          <div class="flex flex-col lg:flex-row" v-if="!noActivities">
             <Stats
               v-if="Object.keys(lastWeekStats).length > 0"
               :stats="lastWeekStats"
@@ -73,8 +100,12 @@ onMounted(() => {
           <h2 class="mt-20 text-center text-3xl font-semibold lg:text-left">
             Goals
           </h2>
-          <p class="mb-20 text-center lg:text-left">
+          <p class="mb-20 text-center lg:text-left" v-if="!noActivities">
             You’re making progress on your personal goals.
+          </p>
+          <p class="mb-20 text-center lg:text-left" v-if="noActivities">
+            You can already add goals which will be evaluated when you upload
+            your first Activity.
           </p>
           <Goals />
         </div>
